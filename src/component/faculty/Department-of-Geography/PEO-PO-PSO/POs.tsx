ca2953f0aa537061565eb803eb09e8de2bf06ecf
sgukop <kiran.dhanawade@sguk.ac.in>
2025-11-22 19:03:58 +0530

    update laest

--- src/component/faculty/Department-of-Geography/PEO-PO-PSO/POs.tsx
+++ src/component/faculty/Department-of-Geography/PEO-PO-PSO/POs.tsx
@@ -10,7 +10,7 @@ const POs = () => {
                         <li><strong>PO1 : </strong>Comprehensive knowledge of geography's core principles, including physical, human, and regional geography.</li>
                         <li><strong>PO2 : </strong>Proficiency in geographical research methods, data collection, and spatial analysis.</li>
                         <li><strong>PO3 : </strong>Ability to apply GIS and remote sensing tools for data visualization and problem-solving.</li>
-                        <li><strong>PO4 : </strong>Development of an ethical and socially responsible approach to geographic research and analysis.</li>
+                        <li><strong>PO4 : </strong>Development of an ethical and socially responsible approach to geographic research and analysis</li>
                     </ul>
             </div>
         </div>
