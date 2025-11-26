1f819d9cff43e47bfbca50eb91e1d8bf782c9e9d
sgukop <kiran.dhanawade@sguk.ac.in>
2025-11-26 17:51:48 +0530

    update cel

--- src/component/photo-gallery/Celebrities/CelebritiesSGU.tsx
+++ src/component/photo-gallery/Celebrities/CelebritiesSGU.tsx
@@ -14,18 +14,26 @@ const ComputerDeptNewEvents = () => {
 
         {/* Helper Function Template */}
         {[
-          { img: "40.jpg", name: "", role: "Actress" },
-          { img: "1.jpg", name: "Prajakta Mali", role: "Actress" },
+          { img: "40.jpeg", name: "", role: "Actress" },
+         
           { img: "2.jpg", name: "", role: "" },
           { img: "3.jpg", name: "Salman Khan", role: "Actor" },
+          { img: "25.jpg", name: "Ajay Devgan", role: "Actor" },
           { img: "4.jpg", name: "Sara Ali Khan", role: "Actress" },
-          { img: "5.jpg", name: "", role: "" },
+
           { img: "6.jpg", name: "Sara Ali Khan", role: "Actress" },
           { img: "7.jpg", name: "", role: "" },
+          { img: "5.jpg", name: "", role: "" },
+         
+          
           { img: "8.jpg", name: "", role: "" },
+           { img: "10.jpg", name: "", role: "" },
           { img: "9.jpg", name: "", role: "" },
-          { img: "10.jpg", name: "", role: "" },
-          { img: "11.jpg", name: "", role: "" },
+          
+           { img: "11.jpg", name: "", role: "" },
+          
+         
+         
           { img: "12.jpg", name: "Arjun Kapoor", role: "Actor" },
           { img: "13.jpg", name: "Arjun Kapoor", role: "Actor" },
           { img: "14.jpg", name: "Arjun Kapoor", role: "Actor" },
@@ -34,25 +42,24 @@ const ComputerDeptNewEvents = () => {
           { img: "17.jpg", name: "Sonali Sinha", role: "Actress" },
           { img: "18.jpg", name: "Navjot Singh Sidhu", role: "Indian Cricketer" },
           { img: "19.jpg", name: "Navjot Singh Sidhu", role: "Indian Cricketer" },
-          { img: "20.jpg", name: "Navjot Singh Sidhu", role: "Indian Cricketer" },
-          { img: "21.jpg", name: "Navjot Singh Sidhu", role: "Indian Cricketer" },
           { img: "22.jpg", name: "", role: "" },
           { img: "23.jpg", name: "", role: "" },
-          { img: "24.jpg", name: "", role: "" },
-          { img: "25.jpg", name: "Ajay Devgan", role: "Actor" },
+          
+          
           { img: "26.jpg", name: "", role: "" },
           { img: "27.jpg", name: "", role: "" },
           { img: "28.jpg", name: "Saif Ali Khan", role: "Actor" },
           { img: "29.jpg", name: "Saif Ali Khan", role: "Actor" },
           { img: "30.jpg", name: " Prasad Oak", role: "Marathi film actors" },
-          { img: "31.jpg", name: "Sara Ali Khan", role: "Actress" },
-          { img: "32.jpg", name: "", role: "" },
+          { img: "24.jpg", name: "", role: "" },
+          
           { img: "33.jpg", name: "", role: "" },
           { img: "34.jpg", name: "", role: "" },
           { img: "35.jpg", name: "", role: "" },
           { img: "36.jpg", name: "", role: "" },
           { img: "37.jpg", name: "", role: "" },
-          { img: "38.jpg", name: "", role: "" }        
+          { img: "38.jpg", name: "", role: "" },
+           { img: "1.jpg", name: "Prajakta Mali", role: "Actress" }       
         ].map((item, idx) => (
           <div className="col-xl-6 col-lg-10" key={idx}>
             <div className="tl-8-course flex flex-col rounded-[20px] h-full shadow-sm p-2 hover:shadow-lg transition duration-300 bg-white">
