3ba6c0a780ace9fc64a3c4df5255cc2a461522b1
sgukop <kiran.dhanawade@sguk.ac.in>
2025-11-27 13:43:49 +0530

    update

--- src/component/photo-gallery/Celebrations/59-Birthday-Celebration/BirthdayCelebration59.tsx
+++ src/component/photo-gallery/Celebrations/59-Birthday-Celebration/BirthdayCelebration59.tsx
@@ -27,7 +27,7 @@ return (
         <h2 className="tl-9-section-title py-4 rounded-tl-[40px] rounded-br-[40px] w-fit px-8 bg-[#4f70b6] font-bold text-white mx-auto mb-50">Birthday Celebration of Hon. President</h2>
         
         <div className="row tl-7-classes-row">
-        <h6 className="tl-8-feature-title">59th Birthday Celebration of Hon. President Mr. Sanjayji Ghodawat in the presence of Mr. Prasad Oak (marathi film actor) on 28th February 2024 at Sanjay Ghodawat University Campus</h6>
+        <h6 className="tl-8-feature-title">59th Birthday Celebration of Hon. President Mr. Sanjayji Ghodawat in the presence of Arjun Kapoor on 28th February 2024 at Sanjay Ghodawat University Campus</h6>
           {celebrationsphotoes.map((Items, index) => (
             <div className="col-lg-4 col-sm-6 rounded-[20px]" key={index}>
               <div className="tl-7-class">
