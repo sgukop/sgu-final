6a2bcc7e89d4e2b4c386d0c293abf09bb25e2e96
sgukop <kiran.dhanawade@sguk.ac.in>
2025-11-29 10:23:47 +0530

    ceh

--- src/component/academics/Curriculum-Structure/che-Dept/bscSyllabus.tsx
+++ src/component/academics/Curriculum-Structure/che-Dept/bscSyllabus.tsx
@@ -8,13 +8,12 @@ const BscSyllabus = () => {
         <Table responsive className="governing-table">
           <thead>
               <tr>
-                  <th><p className="text-[#4f70b6]">B.Sc</p></th>
-                  <th><p className="text-[#4f70b6]">Curriculum Structure and Syllabus</p></th>
+              
               </tr>
           </thead>
           <tbody>
                <tr>
-                  <td><p>Curriculum Structure </p></td>
+                  <td><p>B.Sc</p></td>
                   <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/69298d2321f871c0dce9ae19">Curriculum  Structure and Syllabus<i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
                           
--- src/component/academics/Curriculum-Structure/che-Dept/mscSyllabus.tsx
+++ src/component/academics/Curriculum-Structure/che-Dept/mscSyllabus.tsx
@@ -7,15 +7,14 @@ const MscSyllabus = () => {
         <Table responsive className="governing-table">
           <thead>
               <tr>
-                  <th><p className="text-[#4f70b6]">M.Sc</p></th>
-                  <th><p className="text-[#4f70b6]">Curriculum Structure and Syllabus</p></th>
+                 
               </tr>
           </thead>
           <tbody>
             
               
               <tr>
-                  <td><p>Curriculum Structure </p></td>
+                  <td><p>M.Sc </p></td>
                   <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/69298ced21f871c0dce9ae14">Curriculum Structure and Syllabus <i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
              
