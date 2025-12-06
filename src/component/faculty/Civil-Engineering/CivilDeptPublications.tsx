3ad958f911879efc9ff238c7423b7052e357fe68
sgukop <kiran.dhanawade@sguk.ac.in>
2025-12-06 16:43:34 +0530

    update Publication Civil

--- src/component/faculty/Civil-Engineering/CivilDeptPublications.tsx
+++ src/component/faculty/Civil-Engineering/CivilDeptPublications.tsx
@@ -22,32 +22,101 @@ const CivilDeptPublications = () => {
                         <h3 className="tl-event-details-area-title text-[#4f70b6]">Faculty Publications</h3>
                         <div className="tl-event-details-key-content tl-event-details-area">
                                 <ul className="tl-event-details-key-content-list phd-container">
-                                    <li>Mrs. Adnya Manjrekar published a paper on “Study On Performance Evaluation Of Hybrid Constructed Wetland”, International Journal of Research in Engineering, Science and Management, Volume-3, Issue-6, Month-2020, ISSN (Online): 2581-5719 .</li>
-                                    <li>Mr. C.S. Patil, Dr. V.B. Patil, Dr. S.B. Kharmale., published a paper on “Performance of Steel Moment Resisting Frame with friction Damper in Different Bracing System”, International Journal of Applied Engineering and Research. ISSN 0973-4562, Volume 14, Number 11(2019).</li>
-                                    <li>Mrs. AdnyaManjrekar published a paper on “Waste Plastic Bottles used in Construction”, International Journal of Engineering Development and Research,Volume 7,Issue 4, ISSN:2311-9939, 2019.</li>
-                                    <li>Dr. N.K Patil, Mr. S.K. Kamane, Dr. B.R. Patgundi., published a paper on “Flexural Behaviour of Artificially Degraded SteelI section Externally Bonded with GFRP,BFRP &CFRP” International Journal of recent Technolgy and Engineering.ISSN;2277-3878,Volume 8- Issue 2, July 2019.</li>
-                                    <li>S. S. Shiyekar published paper on " Application of Multi objective Genetic Algorithm to Multipurpose Reservoir " at International Journal for Research in Applied Science & Engineering Technology (IJRASET), Vol. 05, Issue, 01, January, 2018 pp 385-390</li>
-                                    <li>V.S. Patil, Pratima Patil published paper on " Application of Multi objective Genetic Algorithm to Multipurpose Reservoir" at International Journal for Research in Applied Science & Engineering Technology (IJRASET), Vol. 05, Issue, 01, January, 2018 pp 385-390</li>
-                                    <li>C. S. Patil , Joshilkar P D published paper on " Force Displacement Based Design of Steel Moment " at International Research Journal of Engineering and Technology (IRJET), Vol. 06, Issue, 01, January, 2018 pp 1419-1424</li>
-                                    <li>S.S. Shinde published paper on "Experimental Study Of Tensile Strength Of Concrete With Partial Replacement Of Natural Sand With Manufactured Sand" at International Research Journal of Engineering and Technology (IRJET)</li>
-                                    <li>K.A. Sutar, Sameer Shastri published paper on "Grey Water Management by using Stabilization Ponds" at IJSRD - International Journal for Scientific Research & Development| ,Vol. 5, Issue 10, 2017Vol. 5, Issue 10, 2017,pp 639-641</li>
-                                    <li>A. S. Manjarekar published paper on "Construction Safety Management" at International Journal for Research in Applied Science & Engineering Technology (IJRASET), Volume 5 Issue 12, December 2017,pp 2137-2145</li>
-                                    <li>A.A. Koshti published paper on "Behavior of Reinforced Concrete Building with Underground Storey's" at International Journal for Research in Applied Science & Engineering Technology (IJRASET), Volume 5 Issue 12 December 2017,pp 1839-1847</li>
-                                    <li>V.G. Khurd, A S. Dixit, published paper on "Nonlinear Flexural behavior of Post Tensioned beam" at International Journal for Research in Applied Science & Engineering Technology (IJRASET),Volume 5 Issue 11, November 2017,pp 1839-1847</li>
-                                    <li>C. S. Patil ,R S. Shreshthi published paper on "Strength of Reinforced Concrete Beam Using Dual Grade Concrete" at Asian Journal of Science and Technology, Vol. 08, Issue, 11, November, 2017 pp 6598-6604</li>
-                                    <li>P.G. Nidoni published paper on "Incineration Process for Solid Waste Management and Effective Utilization of by Products" at International Research Journal of Engineering and Technology (IRJET)Volume: 04 Issue: 12, Dec-2017, pp 378-382</li>
-                                    <li>V.S. Patil, Yogesh Jadhav, published paper on "Effect of User Defined Plastic Hinges on Nonlinear Modelling of Reinforced Concrete Frame for Seismic Analysis" at International Research Journal of Engineering and Technology (IRJET) Volume: 04 Issue: 12, Dec-2017 pp 28-32</li>
-                                    <li>A D Katdare published paper on "Effect of Depth and Shape of Footing on BearingCapacity of Soil Near Atigre, Kolhapur,Maharashtra" at International Journal for Research in Applied Science & Engineering Technology (IJRASET)Volume 5 Issue 11, November 2017, pp 4032-4033</li>
-                                    <li>D.A. Gunjagi, N.M. Patil ,S.B. Patil , S.B.Kore published paper on "Assessment of Geotechnical Properties of Black Cotton Soil Blended with Lime and Fly-Ash" at International Journal for Research in Applied Science & Engineering Technology (IJRASET) Volume 5 Issue 11, November 2017, pp5018-5023</li>
-                                    <li>V.S. Patil, Milind Waghmare published paper on "Effect of Presence of Infills in Ground Storey on Seismic Performance of R.C. Buildings" at International Research Journal of Engineering and Technology (IRJET), Volume: 04 Issue: 11, Nov -2017 pp1175-1178</li>
-                                    <li>A. S. Manjarekar, A. S. Dixit published paper on "To Study Strength Parameters of Fly Ash Based Geopolymer Concrete" at International Journal for Research in Applied Science & Engineering Technology (IJRASET),Volume 5 Issue VII, July 2017, 1355-1364</li>
-                                    <li>A. S. Manjarekar, A. S. Dixit published paper on "Ferrocement Technology used for Gutter Construction " at International Journal for Research in Applied Science & Engineering Technology (IJRASET),Volume 5 Issue VII, July 2017, 1365-1378</li>
-                                    <li>V. G. Khurd, K. S. Balwan, S. S. Chougule published paper on "To Study The Responces Of Bridge Deck For Various Skew Angles" at International Journal of Research Publications in Engineering and Technology [IJRPET], Volume 3 Issue 9, Sep. -2017,35-37</li>
-                                    <li>V. G. Khurd, K. S. Balwan, S. S. Chougule published paper on "Study Of Bridge Deck A Review" at International Journal of Research Publications in Engineering and Technology [IJRPET] Volume 3 Issue 9, Sep. -2017, 23-25</li>
-                                    <li>S. B. Kore, S. S. Gandhi, M. A. Patel published paper on "Alternative Method for Desalination of Seawater" at International Journal of Engineering Technology Science and Research IJETSR Volume 4, Issue 10 October 2017, 133-135</li>
-                                    <li>A. A. Lole, M. A. Patel, S. S. Gandhi published paper on "Preview of Landslide in Western Ghat" at International Journal of Engineering Technology Science and Research IJETSR Volume 4, Issue 10 October 2017, 130-132</li>
-                                    <li>V.S. Patil and S.N. Tande published paper on "Development of seismic fragility curves for mid-rise masonry infilled reinforced concrete building by incremental</li>
-                                </ul>
+
+                                        <li>Dr. Sachin P. Patil published a paper on “Experimental Study on Acid Effect on Compressive Strength of M40 Concrete with Blast Furnace Slag as a Partial Replacement of Coarse Aggregate”, International Journal of Engineering Trends and Technology, Volume 73, Issue 5, 2025, ISSN 2231-5381.</li>
+
+                                        <li>Dr. Sachin P. Patil published a paper on “Nonlinear Seismic Fragility Assessment of Masonry Infilled RC Frame”, Asian Journal of Civil Engineering, 2025, ISSN 1563-0854.</li>
+
+                                        <li>Dr. Sachin P. Patil published a paper on “Dynamic Behavior of Truss Bridge Structures Subjected to Moving Load”, Journal of Harbin Engineering University (JHEU), 2025, ISSN 1006-7043.</li>
+
+                                        <li>Mr. N. M. Patil published a paper on “Prevention and Mitigation of the Disaster Management for High Rise Buildings by Using RII Method: Kolhapur City”, International Journal of Environmental Sciences, Volume 11, Issue 19S, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Mr. N. M. Patil published a paper on “Analysis of Root Causes of Construction Cost-Overrun in Kolhapur City”, International Journal of Novel Research and Development (IJNRD), Issue May-2025, 2025, ISSN 2456-4184.</li>
+
+                                        <li>Mr. Shripad Bajirao Kore published a paper on “Landslide Hazard Zonation Mapping Near Galel, Along The Mumbai-Goa Highway”, International Journal of Environmental Sciences, Volume 11, Issue 19S, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Mr. Shripad Bajirao Kore published a paper on “Feasibility of Utilizing Plastic Solid Waste for Road Construction”, International Journal of Novel Research and Development (IJNRD), Volume 10, Issue 6, June 2025, ISSN 2456-4184.</li>
+
+                                        <li>Mr. S. B. Kore published a paper on “Remote Sensing and GIS-Based Landslide Susceptibility Mapping in Karul Ghat, Maharashtra”, International Journal of Novel Research and Development (IJNRD), Volume 10, Issue 9, September 2025, ISSN 2456-4184.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Experimental Study on Effect of Blast Furnace Slag as a Partial Replacement of Coarse Aggregate on Compressive Strength of M20 and M30 Grade Concrete”, International Journal of Environmental Sciences, Volume 11, Issue 10, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Effect of Lap Length on the Flexure Strength of RC Beams Using Self-Compacting Concrete”, International Journal of Environmental Sciences, Volume 11, Issue 17, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Experimental Evaluation of Nano Silica Effects on Mechanical Properties of Concrete”, International Journal of Environmental Sciences, Volume 11, Issue 19, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Nonlinear Seismic Fragility Assessment of Masonry Infilled RC Frame”, National Conference on Civil and Mechanical Engineering (NCCME25), Conference Issue, 13 July 2025.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Dynamic Behavior of Truss Bridge Structures Subjected to Moving Load”, National Conference on Civil and Mechanical Engineering (NCCME25), Conference Issue, 13 July 2025.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Nonlinear Seismic Fragility Assessment of Masonry-Infilled RC Frame Structures”, Asian Journal of Civil Engineering, Issue July 2025, ISSN 1563-0854.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Dynamic Behavior of Truss Bridge Structures Subjected to Moving Load”, Journal of Harbin Engineering University, Volume 46, Issue July 2025, ISSN 1006-7043.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Experimental Study on Acid Effect on Compressive Strength of M40 Concrete with Blast Furnace Slag as a Partial Replacement of Coarse Aggregate”, International Journal of Engineering Trends and Technology, Volume 73, Issue May 2025, ISSN 2231-5381.</li>
+
+                                        <li>Dr. Sachin P. Patil published a paper on “Experimental Study on Effect of Utilization of Blast Furnace Slag in M20 and M30 Grade Concrete as a Partial Replacement of Coarse Aggregate”, Journal of Technology, Volume 13, Issue 3, 2025, ISSN 1012-3407.</li>
+
+                                        <li>Dr. Sachin P. Patil published a paper on “Experimental Study on Effect of Blast Furnace Slag as a Partial Replacement of Coarse Aggregate on Compressive Strength of M20 and M30 Grade Concrete Mix”, International Journal of Environmental Sciences, Volume 11, Issue 10, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Effect of Lap Length on the Flexure Strength of RC Beams Using Self-Compacting Concrete”, International Journal of Environmental Sciences, Volume 11, Issue 17, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Experimental Evaluation of Nano Silica Effects on Mechanical Properties of Concrete”, International Journal of Environmental Sciences, Volume 11, Issue 19, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Mr. S. B. Kore published a paper on “Feasibility Study of Partial Replacement of Coarse Aggregate with Plastic in Concrete”, International Journal of Novel Research and Development (IJNRD), Volume 10, Issue 10, June 2025, ISSN 2456-4184.</li>
+
+                                        <li>Ms. Adnya Manjrekar published a paper on “Evaluating Modular Construction: Effect on Resource Efficiency Using MS Project”, International Journal of Environmental Sciences, Volume 11, Issue 18, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Ms. Adnya Manjrekar published a paper on “Performance Evaluation of AAC Blocks Incorporating GSA and Foundry Waste as Partial Replacement”, International Journal of Environmental Sciences, Volume 11, Issue 18, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Mr. S. B. Kore published a paper on “Landslide Hazard Zonation Mapping Near Galel Along the Mumbai-Goa Highway”, International Journal of Environmental Sciences, Volume 11, Issue 19S, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Adnya Manjrekar published a paper on “Critical Study Analysis and Suggestion for Labor Improvement in Construction Industry”, International Journal of Novel Research and Development (IJNRD), Volume 10, Issue 8, August 2025, ISSN 2456-4184.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Fragility Analysis of RC Framed Using Incremental Dynamic Analysis”, Journal of Harbin Engineering University, Volume 46, Issue 9, September 2025, ISSN 1006-7043.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Influence of Soil-Structure Interaction on Seismic Behaviour of Mid-Rise Building Using Non Linear Time History Analysis”, Journal of Harbin Engineering University, Volume 46, Issue 9, September 2025, ISSN 1006-7043.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Influence of Ground Motion Scaling on the Nonlinear Seismic Response of Reinforced Concrete Building”, Journal of Harbin Engineering University, Volume 46, Issue 9, September 2025, ISSN 1006-7043.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Investigation of the Engineering Characteristics of Translucent Concrete”, International Journal of Environmental Sciences, Volume 11, Issue 23S, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Effect of Ductility Factor on the Performance of PEB Industrial Steel Structure”, Journal of Technology, Volume 15, 2025.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Structural Analysis of Blast Resistant Building: Type and Blast Load Considerations”, Journal of Technology, Volume 15, 2025.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Seismic Performance of Tall Building with RC Floating Columns and Shear Wall”, Journal of Technology, Volume 15, 2025.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Comparative Study on Seismic Behavior of RC Structure with Framed Tube Structure and Shear Wall for Multistoried”, International Journal of Novel Research and Development (IJNRD), Volume 10, Issue 7, July 2025, ISSN 2456-4184.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Fragility Analysis of an RC Building Using Plastic Analysis”, International Journal of Novel Research and Development (IJNRD), Volume 10, Issue 8, August 2025, ISSN 2456-4184.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Behavior of Mid-Rise RC Structure Under Blast Loading Using FEM Software”, International Journal for Research in Applied Science & Engineering Technology, Volume 13, Issue August 2025.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Comparative Study of Concrete Properties Using Fiber Reinforcement Polymer”, International Journal of Environmental Sciences, Volume 11, Issue 17S, 2025, ISSN 2229-7359.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Seismic Behavior of High-Rise Building With Outrigger”, International Journal of Novel Research and Development (IJNRD), Volume 9, Issue 8, August 2024, ISSN 2456-4184.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Performance Evaluation of RC Building Subjected to Repeated Earthquake Load”, Reliability Engineering and Resilience, Volume 3, Issue 2, 2022, ISSN 2821-0727.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Damage Assessment of Low Rise to Mid Rise Structure Subjected to Repeated Earthquake Load”, International Journal of Engineering Research Online, 2022.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Petrographic Investigation to Assess Condition of Concrete Exposed to Fire”, International Research Journal of Engineering and Technology, 2022.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Review of Various Provisions in Various Building Seismic Design Codes”, International Research Journal of Engineering and Technology, 2022.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Performance Evaluation of RC Building Subjected to Repeated Earthquake Load”, Reliability Engineering and Resilience, Issue 28 July 2022, ISSN 2821-0727.</li>
+
+                                        <li>Mr. N. M. Patil published a paper on “Value Engineering for Aluminum Formwork and Other Techniques for Innovation in Construction”, IJIRT, Volume 9, Issue 9, November 2022.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Concrete Using Recycled Aggregate”, International Research Journal of Engineering and Technology, 2021.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “Analysis of Concrete Diagrid System with Different Story Module”, International Research Journal of Engineering and Technology, 2021.</li>
+
+                                        <li>Dr. S. P. Patil published a paper on “A Study of Nonlinear Behavior of Multistoried Structure for Repeated Earthquake”, Reliability Engineering and Resilience, Volume 2, Issue 2, 2020, ISSN 2821-0727.</li>
+
+                                    </ul>
+
                         </div>
                     </div>
                 </div>
