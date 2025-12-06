1d478b0b9ed5a3f8e8403f7023316eec9ea95410
sgukop <kiran.dhanawade@sguk.ac.in>
2025-12-06 16:38:55 +0530

    update MBA

--- src/component/academics/Curriculum-Structure/BBA-Dept/MBA.tsx
+++ src/component/academics/Curriculum-Structure/BBA-Dept/MBA.tsx
@@ -14,8 +14,13 @@ const MBA = () => {
           <tbody>
               <tr>
                   <td><p>MBA Syllabus </p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/69340db0914ce31b3c5b9c8e"> R25_I_II_Full_Syllabus_2025. <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                 
+             </tr>
+             <tr>
+                 <td><p>MBA Syllabus </p></td>
                   <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/686e24bcab9e6efa3e79e32c">MBA Syllabus  <i className="fa-regular fa-arrow-right-long"></i></a></td>
-                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/686e25f2ab9e6efa3e79e337">R25 Sem I & Sem I  <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                  
              </tr>
              
 
