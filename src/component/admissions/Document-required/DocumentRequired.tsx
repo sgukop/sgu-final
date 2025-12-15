150da3d236192b874cc4515b2b444658c0568d51
sgukop <kiran.dhanawade@sguk.ac.in>
2025-12-15 15:22:23 +0530

    update required Documents

--- src/component/admissions/Document-required/DocumentRequired.tsx
+++ src/component/admissions/Document-required/DocumentRequired.tsx
@@ -1,85 +1,101 @@
 import React from "react";
 
+type DocumentSection = {
+  title: string;
+  documents: string[];
+};
+
+const documentData: DocumentSection[] = [
+  {
+    title:
+      "For First Year B. Tech / Direct Second Year B. Tech / First Year & Direct Second Year B. Pharmacy Admissions",
+    documents: [
+      "CET / JEE / State Entrance Test Score Card (F. Y. B. Tech / B. Pharmacy)",
+      "SSC Marksheet",
+      "HSC Marksheet / Diploma Marksheet",
+      "Leaving / Transfer Certificate",
+      "Domicile and Nationality Certificate",
+      "Migration Certificate (HSC Board other than Maharashtra State Board)",
+      "Caste Certificate (if applicable)",
+      "3 Colour Photos",
+      "2 Photocopies of Above original Documents",
+    ],
+  },
+  {
+    title: "For First Year M. Tech / MBA / M. Sc / M.C.A. Admissions",
+    documents: [
+      "GATE / CET / ATMA / CAT / SGUEE",
+      "SSC Marksheet",
+      "HSC Marksheet / Diploma Marksheet",
+      "Degree Final Year Marksheet",
+      "Leaving / Transfer Certificate",
+      "Domicile and Nationality Certificate",
+      "Migration Certificate",
+      "Caste Certificate (if applicable)",
+      "3 Colour Photos",
+      "2 Photocopies of Above original Documents",
+    ],
+  },
+  {
+    title:
+      "For First Year BBA / B. Com / BA / B.C.A / D. Pharmacy / B.Sc / B.Sc Food Science and Technology / B.M.L.T. Admissions",
+    documents: [
+      "SSC Marksheet",
+      "HSC Marksheet",
+      "Leaving Certificate",
+      "Domicile and Nationality Certificate",
+      "Migration Certificate (HSC Board other than Maharashtra State Board)",
+      "Caste Certificate (if applicable)",
+      "3 Colour Photos",
+      "2 Photocopies of Above original Documents",
+    ],
+  },
+  {
+    title: "For First Year B.Design",
+    documents: [
+      "SGUEED / UCEED / PERA CET",
+      "SSC Marksheet",
+      "HSC Marksheet / Diploma Marksheet",
+      "Leaving / Transfer Certificate",
+      "Domicile and Nationality Certificate",
+      "Migration Certificate (HSC Board other than Maharashtra State Board)",
+      "Caste Certificate (if applicable)",
+      "3 Colour Photos",
+      "2 Photocopies of Above original Documents",
+    ],
+  },
+];
+
 const DocumentRequired = () => {
-   return(
-    <div className="tl-event-details-container pt-100 pb-100 tl-10-about-section" >
-        <div className="container">
-            <div className="row tl-event-details-row g-0">
-                <div className="col-lg-12">
-                    <h2 className="universityobject-heading dark-mode-white-color mb-50">Documents Required for Admissions 2025-26</h2>
-                    <div className="tl-event-details-left">
-                        <div className="tl-event-details-key-content tl-event-details-area">
-                            <h4 className="dark-mode-white-color mb-30">For First Year B. Tech / Direct Second Year B. Tech /First Year & Direct Second Year B. Pharmacy Admissions</h4>
-                               <ul className="tl-event-details-key-content-list phd-container">
-                                    <li>CET / JEE/ State Entrance Test Score Card (F. Y. B. Tech / B. Pharmacy)</li>
-                                    <li>SSC Marksheet</li>
-                                    <li>HSC Marksheet / Diploma Marksheet</li>
-                                    <li>Leaving / Transfer Certificate</li>
-                                    <li>Domicile and Nationality Certificate</li>
-                                    <li>Migration Certificate ( HSC Board other than Maharashtra State Board)</li>
-                                    <li>Caste Certificate ( if applicable)</li>
-                                    <li>3 Colour Photos</li>
-                                    <li>2 Photocopies of Above original Documents</li>
-                                </ul>
-                            </div>
-                        </div>
-                        <div className="tl-event-details-left mt-30">
-                        <div className="tl-event-details-key-content tl-event-details-area">
-                            <h4 className="dark-mode-white-color mb-30">For First Year M. Tech / MBA / M. Sc / M.C.A. Admissions</h4>
-                               <ul className="tl-event-details-key-content-list phd-container">
-                                    <li>GATE / CET / ATMA / CAT / SGUEE</li>
-                                    <li>SSC Marksheet</li>
-                                    <li>HSC Marksheet / Diploma Marksheet</li>
-                                    <li>Degree Final Year Marksheet</li>
-                                    <li>Leaving / Transfer Certificate</li>
-                                    <li>Domicile and Nationality Certificate</li>
-                                    <li>Migration Certificate</li>
-                                    <li>Caste Certificate (if applicable)</li>
-                                    <li>3 Colour Photos</li>
-                                    <li>2 Photocopies of Above original Documents</li>
-                                </ul>
-                            </div>
-                        </div>
+  return (
+    <div className="tl-event-details-container pt-100 pb-100 tl-10-about-section">
+      <div className="container">
+        <div className="row tl-event-details-row g-0">
+          <div className="col-lg-12">
+            <h2 className="universityobject-heading dark-mode-white-color mb-50">
+              Documents Required for Admissions 2025-26
+            </h2>
 
-                        <div className="tl-event-details-left mt-30">
-                        <div className="tl-event-details-key-content tl-event-details-area">
-                            <h4 className="dark-mode-white-color mb-30">For First Year BBA / B. Com / BA / B.C.A / D. Pharmacy/B.Sc/B.Sc Food Science and Technology/ B.M.L.T. Admissions</h4>
-                               <ul className="tl-event-details-key-content-list phd-container">
-                                    <li>SSC Marksheet</li>
-                                    <li>HSC Marksheet</li>
-                                    <li>Leaving Certificatet</li>
-                                    <li>Domicile and Nationality Certificate</li>
-                                    <li>Migration Certificate ( HSC Board other than Maharashtra State Board )</li>
-                                    <li>Caste Certificate (if applicable)</li>
-                                    <li>3 Colour Photos</li>
-                                    <li>2 Photocopies of Above original Documents</li>
-                                   
-                                </ul>
-                            </div>
-                        </div>
-
-                        <div className="tl-event-details-left mt-30">
-                        <div className="tl-event-details-key-content tl-event-details-area">
-                            <h4 className="dark-mode-white-color mb-30">For First Year B.Design</h4>
-                               <ul className="tl-event-details-key-content-list phd-container">
-                                    <li>SGUEED /Uceed / Pera Cet</li>
-                                    <li>SSC Marksheet</li>
-                                    <li>HSC Marksheet / Diploma Marksheet</li>
-                                    <li>Leaving / Transfer Certificate</li>
-                                    <li>Domicile and Nationality Certificate</li>
-                                    <li>Migration Certificate ( HSC Board other than Maharashtra State Board)</li>
-                                    <li>Caste Certificate ( if applicable)</li>
-                                    <li>3 Colour Photos</li>
-                                    <li>2 Photocopies of Above original Documents</li>
-                                </ul>
-                            </div>
-                        </div>
-                      
-                    </div>
+            {documentData.map((section, index) => (
+              <div className="tl-event-details-left mt-30" key={index}>
+                <div className="tl-event-details-key-content tl-event-details-area">
+                  <h4 className="dark-mode-white-color mb-30">
+                    {section.title}
+                  </h4>
+                  <ul className="tl-event-details-key-content-list phd-container">
+                    {section.documents.map((doc, i) => (
+                      <li key={i}>{doc}</li>
+                    ))}
+                  </ul>
                 </div>
-            </div>
+              </div>
+            ))}
+            
+          </div>
         </div>
-   )
-}
+      </div>
+    </div>
+  );
+};
 
-export default DocumentRequired
+export default DocumentRequired;
