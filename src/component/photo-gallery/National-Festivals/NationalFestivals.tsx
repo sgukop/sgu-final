ce0b6b631e097a55fa2840c3c3c441da2b71c229
sgukop <kiran.dhanawade@sguk.ac.in>
2025-12-19 16:50:35 +0530

    update republic day 2025

--- src/component/photo-gallery/National-Festivals/NationalFestivals.tsx
+++ src/component/photo-gallery/National-Festivals/NationalFestivals.tsx
@@ -9,7 +9,7 @@ const NationalFestivals = () => {
                 <div className="row gy-4 gy-sm-5 tl-1-blogs-row">
                     <div className="col-lg-4">
                         <div className="tl-1-blog flex flex-col justify-center h-full">
-                            <img src="assets/images/photo-gallery/national-festivals/republic-day-2025/6.JPEG" alt="National Festivals Image" className="tl-1-blog-img-new"/>
+                            <img src="assets/images/photo-gallery/national-festivals/republic-day-2025/10.JPG" alt="National Festivals Image" className="tl-1-blog-img-new"/>
                                 <div className="tl-1-blog-txt">
                                     <h4 className="tl-1-blog-title"><Link href="/photoes-republic-day-2025">26 January Republic day 2025</Link></h4>
                                     <Link href="/photoes-republic-day-2025" className="tl-1-blog-btn">Continue Reading <i className="fa-regular fa-arrow-right"></i></Link>
