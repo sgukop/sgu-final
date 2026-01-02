ae99c820613da202bae32cec6e278bb1a951ffe7
sgukop <kiran.dhanawade@sguk.ac.in>
2026-01-02 14:26:39 +0530

    update CSE

--- src/component/faculty/Computer-Science-Engineering/ComputerDeptFaculty.tsx
+++ src/component/faculty/Computer-Science-Engineering/ComputerDeptFaculty.tsx
@@ -255,6 +255,19 @@ const ComputerDeptFaculty = () => {
                             </div>
                         </div>
                     </div>
+                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
+                        <div className="tl-1-course-new">
+                            <div className="tl-1-course-img-new">
+                                <img src="/assets/images/faculty-profile-photos/female.jpg" alt="Faculty Image"/>
+                            </div>
+
+                            <div className="tl-1-course-txt">
+                                <h4 className="tl-1-course-title-new mt-30">Ms. Sana Juber Nadaf</h4>
+                                <p className="tl-1-course-author-new dark-mode-white-color">Teaching Assistant</p>
+                                <Link href="#" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
+                            </div>
+                        </div>
+                    </div>
                     
                     
                   
