be83d9bfb755fcbfeaefc5cb4b43ed26f5f70cb3
sgukop <kiran.dhanawade@sguk.ac.in>
2026-01-30 11:43:29 +0530

    cse Staff remove

--- src/component/faculty/Computer-Science-Engineering/ComputerDeptFaculty.tsx
+++ src/component/faculty/Computer-Science-Engineering/ComputerDeptFaculty.tsx
@@ -203,19 +203,7 @@ const ComputerDeptFaculty = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/cse/female.jpg" alt="Faculty Image"/>
-                            </div>
 
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Mr. Siddappa Balikar</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Teaching Assistant</p>
-                                <Link href="https://manage-api.sguk.ac.in/api/assets/692695ee160c943cf4224b2f" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div>
                     <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
                         <div className="tl-1-course-new">
                             <div className="tl-1-course-img-new">
