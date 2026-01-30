35abb12798b2a7f3f2152fbba6da3c481abd65b0
sgukop <kiran.dhanawade@sguk.ac.in>
2026-01-30 11:28:55 +0530

    update MBA Hod Photo

--- src/component/faculty/School-of-Management/ManagementDeptAbout.tsx
+++ src/component/faculty/School-of-Management/ManagementDeptAbout.tsx
@@ -69,10 +69,10 @@ const ManagementDeptAbout = () => {
               <div className="col-lg-5">
                 <div className="tl-14-about-img">
                   <img
-                    src="assets/images/management-dept/about-department/hod-photo.png"
+                    src="assets/images/management-dept/about-department/hod-photo.jpeg"
                     alt="HOD Image"
                     className="rounded-full"
-                    style={{border:"10px solid #4f70b6", padding:"20px"}}
+                    style={{border:"10px solid #4f70b6", padding:"10px"}}
                   />
                 </div>
               </div>
