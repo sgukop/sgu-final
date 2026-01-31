8510d534f0a66c93aaa162dd3cf67f451be371bb
sgukop <kiran.dhanawade@sguk.ac.in>
2026-01-31 20:43:16 +0530

    update AIML

--- src/component/faculty/Artificial-Intelligence-Machine-Learning/AIMLDeptActivities.tsx
+++ src/component/faculty/Artificial-Intelligence-Machine-Learning/AIMLDeptActivities.tsx
@@ -15,13 +15,14 @@ const AIMLDeptActivities = () => {
             data-target="#menu-navbar"
             data-offset="0"
           >
+             <h3 className="tl-event-details-area-title">Parents Meeting 2025-26</h3>
            <div className="row g-3 g-lg-4 justify-content-center">
             <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/photo-gallery/academics/promptutsav-aiml-2025/2.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Parents Meeting 2025-26</h3>
+                        
                     </div>
                   </div>
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
@@ -29,7 +30,7 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/photo-gallery/academics/promptutsav-aiml-2025/5.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Parents Meeting 2025-26</h3>
+                       
                     </div>
                   </div>
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
@@ -37,15 +38,18 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/photo-gallery/academics/promptutsav-aiml-2025/33.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Parents Meeting 2025-26</h3>
+                       
                     </div>
                   </div>
+                  </div>
+             <h3 className="tl-event-details-area-title">Promptutsav 2025-26</h3>
+             <div className="row g-3 g-lg-4 lift-content-center">
             <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/photo-gallery/academics/promptutsav-aiml-2025/9.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Promptutsav 2025-26</h3>
+                        
                     </div>
                   </div>
             <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
@@ -53,7 +57,7 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/photo-gallery/academics/promptutsav-aiml-2025/1.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Promptutsav 2025-26</h3>
+                        
                     </div>
                   </div>
                   
@@ -62,7 +66,7 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/photo-gallery/academics/promptutsav-aiml-2025/11.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Promptutsav 2025-26</h3>
+                        
                     </div>
                   </div>
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
@@ -70,17 +74,18 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/photo-gallery/academics/promptutsav-aiml-2025/22.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Promptutsav 2025-26</h3>
+                       
                     </div>
                   </div>
-                  
-                  
+                  </div>
+                <h3 className="tl-event-details-area-title">F.Y. AIML Orientation</h3>  
+               <div className="row g-3 g-lg-4 lift-content-center">
                <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                             <figure><img src="assets/images/AIML/activities&achievements/AIML01.jpeg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">F.Y. AIML Orientation</h3>
+                        
                     </div>
                   </div>
 
@@ -89,18 +94,19 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML02.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">F.Y. AIML Orientation</h3>
+                        
                     </div>
                   </div>
-
+                </div>
                   
-
+                <h3 className="tl-event-details-area-title">F.Y. AIML Industrial Visit</h3>
+                  <div className="row g-3 g-lg-4 lift-content-center">
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML05.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">F.Y. AIML Industrial Visit</h3>
+                        
                     </div>
                   </div>
 
@@ -109,16 +115,18 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML06.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">F.Y. AIML Industrial Visit</h3>
+                        
                     </div>
                   </div>
-
+                  </div>
+                  <h3 className="tl-event-details-area-title">AIML SA INAUGURATION</h3>
+                  <div className="row g-3 g-lg-4 lift-content-center">
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML07.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">AIMLSA INAUGURATION</h3>
+                       
                     </div>
                   </div>
 
@@ -127,30 +135,13 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML08.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">AIMLSA INAUGURATION</h3>
+                        
                     </div>
                   </div>
-
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog"> 
-                        <div className="tl-9-event-img">
-                        <figure><img src="assets/images/AIML/activities&achievements/AIML09.jpg" alt="Activities & Achievements Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Teachers Day Celebration</h3>
-                    </div>
                   </div>
-
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog"> 
-                        <div className="tl-9-event-img">
-                        <figure><img src="assets/images/AIML/activities&achievements/AIML10.jpg" alt="Activities & Achievements Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Department meet</h3>
-                    </div>
-                  </div>
-
                   
-
+                  <h3 className="tl-event-details-area-title">Other Activities</h3>
+                  <div className="row g-3 g-lg-4 lift-content-center">
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
@@ -169,13 +160,15 @@ const AIMLDeptActivities = () => {
                         <h3 className="tl-6-latest-article-title mt-10">BOS Meeting</h3>
                     </div>
                   </div>
-
+                  </div>
+            <h3 className="tl-event-details-area-title">YOGA WORKSHOP</h3>
+                  <div className="row g-3 g-lg-4 lift-content-center">
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML15.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">YOGA WORKSHOP</h3>
+                        
                     </div>
                   </div>
 
@@ -184,7 +177,7 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML16.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">YOGA WORKSHOP</h3>
+                        
                     </div>
                   </div>
 
@@ -193,7 +186,7 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML17 (1).jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Ganesha Workshop</h3>
+                       
                     </div>
                   </div>
 
@@ -202,16 +195,18 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML18.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Ganesha Workshop</h3>
+                        
                     </div>
                   </div>
-
+                  </div>
+                  <h3 className="tl-event-details-area-title">Guest Lecture </h3>
+                  <div className="row g-3 g-lg-4 lift-content-center">
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML19.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Guest Lecture for Big Data Analytics</h3>
+                        
                     </div>
                   </div>
 
@@ -220,7 +215,7 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML20.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Guest Lecture for Big Data Analytics</h3>
+                        
                     </div>
                   </div>
 
@@ -229,25 +224,24 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML21.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Guest Lecture for Deep Learning</h3>
-                    </div>
+                          </div>
                   </div>
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML22.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Guest Lecture for Deep Learning</h3>
-                    </div>
+                                </div>
                   </div>
-
+                  </div>
+          <h3 className="tl-event-details-area-title">Hackathon winners</h3>
+                 <div className="row g-3 g-lg-4 lift-content-center">
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
                     <div className="tl-9-blog"> 
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML24.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Hackathon winners</h3>
-                    </div>
+                            </div>
                   </div>
 
                   <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
@@ -255,10 +249,34 @@ const AIMLDeptActivities = () => {
                         <div className="tl-9-event-img">
                         <figure><img src="assets/images/AIML/activities&achievements/AIML25.jpg" alt="Activities & Achievements Image"/></figure>
                         </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Hackathon winners</h3>
+                         </div>
+                  </div>
+                  </div> 
+                  <h3 className="tl-event-details-area-title">Teachers Day Celebration</h3>
+                  <div className="row g-3 g-lg-4 lift-content-center">
+                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
+                    <div className="tl-9-blog"> 
+                        <div className="tl-9-event-img">
+                        <figure><img src="assets/images/AIML/activities&achievements/AIML09.jpg" alt="Activities & Achievements Image"/></figure>
+                        </div>
+                        
                     </div>
                   </div>
-                  </div>   
+                  </div>
+                  
+                  <h3 className="tl-event-details-area-title">Department meet</h3>
+                  <div className="row g-3 g-lg-4 lift-content-center">
+                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
+                    <div className="tl-9-blog"> 
+                        <div className="tl-9-event-img">
+                        <figure><img src="assets/images/AIML/activities&achievements/AIML10.jpg" alt="Activities & Achievements Image"/></figure>
+                        </div>
+                       
+                    </div>
+                  </div>
+                  
+
+                  </div>  
                   <div className="tl-8-course mt-30">
                       <div className="tl-8-course-img">
                           <img src="assets/images/AIML/activities&achievements/AIML23.jpg" alt="Activities & Achievements Image"/>
