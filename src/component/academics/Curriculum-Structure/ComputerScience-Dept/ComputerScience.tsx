f86d8209848a19137d4fd882359abc511dc2a2e4
sgukop <kiran.dhanawade@sguk.ac.in>
2026-02-10 10:15:58 +0530

    update CSE syllabus

--- src/component/academics/Curriculum-Structure/ComputerScience-Dept/ComputerScience.tsx
+++ src/component/academics/Curriculum-Structure/ComputerScience-Dept/ComputerScience.tsx
@@ -13,24 +13,32 @@ const ComputerScience = () => {
           </thead>
           <tbody>
               <tr>
-                  <td><p>First year B.Tech CSE</p></td>
-                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68676dbd59d42ad384df3c25">	First year B.Tech Structure and syllabus_Computer Science and Engineering_2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                  <td><p>FCSE_OE_MM_NEP_22_27</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/698ab721496356092bc64453">CSE_OE_MM_NEP_22_27<i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
              <tr>
-                  <td><p>Second Year B.Tech CSE</p></td>
-                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68676d8259d42ad384df3c1b">Second Year B.Tech Structure and syllabus_Computer Science and Engineering_2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                  <td><p>R22 Final Year B. Tech Structure and syllabus _Computer Science and Engineering AY_2025_26</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/698ab732496356092bc64458">R22 Final Year B. Tech Structure and syllabus _Computer Science and Engineering AY_2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
              <tr>
-                  <td><p>Third Year B. Tech. CSE</p></td>
-                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68676da559d42ad384df3c20">Third Year B.Tech Structure_Computer Science and Engineering_2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                  <td><p>R23 Third Year B.Tech Curriculum Structure and syllabus_Computer Science and Engineering_AY 2025_26</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/698ab7d9496356092bc64474">R23 Third Year B.Tech Curriculum Structure and syllabus_Computer Science and Engineering_AY 2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
              <tr>
-                  <td><p>Final Year B. Tech Structure and syllabus CSE</p></td>
-                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68676d6659d42ad384df3c16">Final Year B.Tech Structure_Computer Science and Engineering_2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                  <td><p>R23_FY_SY_TY_Final_Structure</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/698ab74c496356092bc64460">R23_FY_SY_TY_Final_Structure <i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
              <tr>
-                  <td><p>Final Year B. Tech Structure CSE </p></td>
-                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68676de759d42ad384df3c2a">Final Year B.Tech Structure_Computer Science and Engineering_2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                  <td><p>R23_Second Year B.Tech Structure and syllabus_Computer Science and Engineering_AY_2025_26</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/698ab7c9496356092bc6446f">R23_Second Year B.Tech Structure and syllabus_Computer Science and Engineering_AY_2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+             </tr>
+             <tr>
+                  <td><p>R25_Final_Structure </p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/698ab761496356092bc64465">R25_Final_Structure <i className="fa-regular fa-arrow-right-long"></i></a></td>
+             </tr>
+             <tr>
+                  <td><p>R25_First year B.Tech Structure and syllabus_Computer Science and Engineering_AY 2025_26 </p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/698ab7ba496356092bc6446a">R25_First year B.Tech Structure and syllabus_Computer Science and Engineering_AY 2025_26 <i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
               
           </tbody>
