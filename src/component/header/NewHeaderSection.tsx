bb1a170b47bd392bf6f1b909f88100a3b4d3fad3
sgukop <kiran.dhanawade@sguk.ac.in>
2026-02-16 18:31:25 +0530

    up

--- src/component/header/NewHeaderSection.tsx
+++ src/component/header/NewHeaderSection.tsx
@@ -357,6 +357,10 @@ const NewHeaderSection = () => {
 </div>*/}
 
     <Marquee speed={100} gradient={false} pauseOnHover className="flex m-2">
+
+      <sup><img src="assets/images/new-icon-image.gif"/></sup>
+      <a href="https://www.sguk.ac.in/careers" target="_blank" className="text-[#800000] text-bold hover:text-[#212f64] mr-6"> Current Openings 2026</a>
+      
       <sup><img src="assets/images/new-icon-image.gif"/></sup>
       <a href="https://manage-api.sguk.ac.in/api/assets/69578ac6c03a7929c6c93c73" target="_blank" className="text-[#800000] text-bold hover:text-[#212f64] mr-6"> SGU Student Selected as Top 10 Finalist in National Innovation Challenge Powered by Google Gemini</a>
       
