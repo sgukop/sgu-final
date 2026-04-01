35fa516a70657a4931019ac5e20cfb933b877d54
sgukop <kiran.dhanawade@sguk.ac.in>
2026-04-01 11:57:28 +0530

    update E&TC

--- src/component/faculty/Electrical-Electronics-Engineering/ElectricalDeptFaculty.tsx
+++ src/component/faculty/Electrical-Electronics-Engineering/ElectricalDeptFaculty.tsx
@@ -22,47 +22,11 @@ const ElectricalDeptFaculty = () => {
           >
             <div className="tl-course-details-top-courses">
                 <div className="row g-4">
-                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/electrical/1. Dr. RAJIN LINUS.jpg" alt="Faculty Image"/>
-                            </div>
+                    
 
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Dr Rajin M. Linus</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Associate Professor</p>
-                                <Link href="https://manage-api.sguk.ac.in/api/assets/674ecfeb4b17e19d8f36170e" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div>  
+                     
 
-                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/male.jpg" alt="Faculty Image"/>
-                            </div>
 
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Mr.Uday Tanajirao Patil</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Assistant Professor</p>
-                                <Link href="https://manage-api.sguk.ac.in/api/assets/674ed0004b17e19d8f361713" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div>  
-
-                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/male.jpg" alt="Faculty Image"/>
-                            </div>
-
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Mr.Indrajit Dattatray Pharne</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Assistant Professor</p>
-                                <Link href="https://manage-api.sguk.ac.in/api/assets/674ed0144b17e19d8f361718" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div> 
 
                     <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
                         <div className="tl-1-course-new">
