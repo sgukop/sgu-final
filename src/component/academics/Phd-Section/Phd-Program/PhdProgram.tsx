12a77fc5f1916dedcb059707478910847811b125
sgukop <kiran.dhanawade@sguk.ac.in>
2026-04-07 11:08:28 +0530

    update PHD & PG Section

--- src/component/academics/Phd-Section/Phd-Program/PhdProgram.tsx
+++ src/component/academics/Phd-Section/Phd-Program/PhdProgram.tsx
@@ -1,47 +1,97 @@
 import React from "react";
 
 const PhdProgram = () => {
-   return(
-    <div className="tl-event-details-container pt-100 pb-100" >
-        <div className="container">
-            <div className="row tl-event-details-row g-0">
-                <div className="col-lg-12">
-                    <h2 className="universityobject-heading dark-mode-white-color py-4 rounded-tl-[40px] rounded-br-[40px] w-fit px-8 bg-[#4f70b6] font-bold text-white mx-auto mb-50">PG & Ph.D. Section</h2>
-                    <div>
-                        <div className="tl-event-details-area rounded-lg mb-8 shadow-[2px_2px_0_3px_rgba(242,97,34,0.6)]">
-                            <ul className="tl-event-details-key-content-list phd-admission-process">
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/69213e78f1958828aacf267f">Ph.D. Admission Notification 2025-26 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68ff47d09b0f316357c64c2a">SGU Ph.D Admission Information Brochure <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68ff47df9b0f316357c64c2f">Vacancy List 2025 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68ff47f19b0f316357c64c34">PhD Supervisor List Oct. 2025.<i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/69268524160c943cf4224af0">SGU-PET Syllabus <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68ff480d9b0f316357c64c3e">Instructions for SGUPET 2025 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68ff481a9b0f316357c64c43">Ph.D. Ordinance 2024 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://forms.gle/bxybYx5MxLSxGHGS9">Link for Application Form <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/693a61745fea6e5500e4d837">List of Eligible Candidates  SGUPET 2025 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/693a61845fea6e5500e4d83c">List of Exempted Candidates  SGUPET 2025 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/693cf28f475c25da4f121c31">APS Guidelines <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/693cf2b1475c25da4f121c36">SGUK_Circular APS 2026 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/693cf2db475c25da4f121c3b">SGU-PET Syllabus-2 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/files/Results- SGUPET 2025.pdf">Results- SGUPET 2025 <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/Six month progress report submission .pdf">Six Month Progress Report Submission (PDF) <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/Six month progress report submission .docx"> Six Month Progress Report Submission (DOCX) <i className="fa-regular fa-arrow-right-long"></i> </a> </li> 
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/APS Guidelines .pdf">Rules & Guidelines Governing Annual Progress Seminars (PDF) <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/APS Guidelines .docx">  Rules & Guidelines Governing Annual Progress Seminars (DOCX) <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/APS report cum extension of Ph. D registration form.pdf">APS Report cum Extension of Ph.D. Registration Form (PDF) <i className="fa-regular fa-arrow-right-long"></i> </a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/APS report cum extension of Ph. D registration form.docx">APS Report cum Extension of Ph.D. Registration Form (DOCX) <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/Manual_Phd-Synosis.pdf">Manual for Ph.D. Synopsis (PDF) <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/Manual_Phd-Synosis.docx">Manual for Ph.D. Synopsis (DOCX) <i className="fa-regular fa-arrow-right-long"></i> </a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/Guidelines for preparation of Theses Dissertations Reports.pdf">Guidelines for Preparation of Theses/Dissertations/Reports (PDF) <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                    <li className="mt-2"><a rel="noopener noreferrer" className="dark-mode-white-color font-semibold" target="_blank" href="/assets/phd/Guidelines for preparation of Theses Dissertations Reports.docx">Guidelines for Preparation of Theses/Dissertations/Reports (DOCX) <i className="fa-regular fa-arrow-right-long"></i></a></li>
-                                </ul>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-   )
-}
+  return (
+    <div className="tl-event-details-container pt-100 pb-100">
+      <div className="container">
+        <div className="row tl-event-details-row g-0">
+          <div className="col-lg-12">
 
-export default PhdProgram
+            <h2 className="universityobject-heading dark-mode-white-color py-4 rounded-tl-[40px] rounded-br-[40px] w-fit px-8 bg-[#4f70b6] font-bold text-white mx-auto mb-50">
+              Ph.D. Section
+            </h2>
+
+            {/* ================= PDF SECTION ================= */}
+            <div className="tl-event-details-area rounded-lg mb-8 shadow-[2px_2px_0_3px_rgba(242,97,34,0.6)] p-4">
+              <h4 className="font-bold mb-3 text-[#4f70b6]">📄 PDF Files</h4>
+              <ul className="tl-event-details-key-content-list phd-admission-process">
+
+                <li><a target="_blank" href="/assets/files/phd/01_Research Proposal Template.pdf">01_Research Proposal Template</a></li>
+                <li><a target="_blank" href="/assets/files/phd/02_Approved Ph. D Six Month Progress Report.pdf">02_Approved Ph. D Six Month Progress Report</a></li>
+                <li><a target="_blank" href="/assets/files/phd/03_Approved Six-month PhD Progress Report Submission.pdf">03_Approved Six-month PhD Progress Report Submission</a></li>
+                <li><a target="_blank" href="/assets/files/phd/04_Approved PhD APS Guidelines.pdf">04_Approved PhD APS Guidelines</a></li>
+                <li><a target="_blank" href="/assets/files/phd/05_Approved APS Report cum Extension of Ph. D Registration_Pre-PhD Seminar Form.pdf">05_Approved APS Report cum Extension of Ph. D Registration_Pre-PhD Seminar Form</a></li>
+                <li><a target="_blank" href="/assets/files/phd/06_Aproved Manual_Phd-Synosis.pdf">06_Aproved Manual_Phd-Synosis</a></li>
+                <li><a target="_blank" href="/assets/files/phd/07_Approved PhD Synopsis Template.pdf">07_Approved PhD Synopsis Template</a></li>
+                <li><a target="_blank" href="/assets/files/phd/08_Approved Guidelines for Peparation of PhD Thesis.pdf">08_Approved Guidelines for Peparation of PhD Thesis</a></li>
+                <li><a target="_blank" href="/assets/files/phd/09_Approved SGU Ph.D. Thesis Template.pdf">09_Approved SGU Ph.D. Thesis Template</a></li>
+                <li><a target="_blank" href="/assets/files/phd/10_Approved PhD Evaluater Appointment Letter.pdf">10_Approved PhD Evaluater Appointment Letter</a></li>
+                <li><a target="_blank" href="/assets/files/phd/11_Approved Ph.D Adjudication Report.pdf">11_Approved Ph.D Adjudication Report</a></li>
+                <li><a target="_blank" href="/assets/files/phd/12_Approved PhD Evaluation of Thesis Letter.pdf">12_Approved PhD Evaluation of Thesis Letter</a></li>
+                <li><a target="_blank" href="/assets/files/phd/13_Approved PhD Examination Remulneration Bill.pdf">13_Approved PhD Examination Remulneration Bill</a></li>
+                <li><a target="_blank" href="/assets/files/phd/14_CHECK LIST T0 BE SUBMITTED WITH THESIS.pdf">14_CHECK LIST T0 BE SUBMITTED WITH THESIS</a></li>
+                                
+              </ul>
+            </div>
+
+            {/* ================= WORD SECTION ================= */}
+            <div className="tl-event-details-area rounded-lg mb-8 shadow-[2px_2px_0_3px_rgba(79,112,182,0.6)] p-4">
+              <h4 className="font-bold mb-3 text-[#f26122]">📝 Word Files (DOCX)</h4>
+              <ul className="tl-event-details-key-content-list phd-admission-process">
+
+                <li><a target="_blank" href="/assets/files/phd/01_Re.search Proposal  Template.docx">01_Re.search Proposal  Template</a></li>
+                <li><a target="_blank" href="/assets/files/phd/02_Approved Ph. D Six Month Progress Report.docx">02_Approved Ph. D Six Month Progress Report</a></li>
+                <li><a target="_blank" href="/assets/files/phd/03_Approved Six-month PhD Progress Report Submission.docx">03_Approved Six-month PhD Progress Report Submission</a></li>
+                <li><a target="_blank" href="/assets/files/phd/04_Approved PhD APS Guidelines.docx">04_Approved PhD APS Guidelines</a></li>
+                <li><a target="_blank" href="/assets/files/phd/05_Approved APS Report cum Extension of Ph. D Registration_Pre-PhD Seminar Form.docx">05_Approved APS Report cum Extension of Ph. D Registration_Pre-PhD Seminar Form</a></li>
+                <li><a target="_blank" href="/assets/files/phd/06_Aproved  Manual_Phd-Synosis.docx">06_Aproved  Manual_Phd-Synosis</a></li>
+                <li><a target="_blank" href="/assets/files/phd/07_Approved PhD Synopsis Template.docx">07_Approved PhD Synopsis Template</a></li>
+                <li><a target="_blank" href="/assets/files/phd/08_Approved Guidelines for Peparation of PhD Thesis.docx">08_Approved Guidelines for Peparation of PhD Thesis</a></li>
+                <li><a target="_blank" href="/assets/files/phd/09_Approved SGU Ph.D. Thesis Template.docx">09_Approved SGU Ph.D. Thesis Template</a></li>
+                <li><a target="_blank" href="/assets/files/phd/10_Approved PhD Evaluater Appointment Letter.docx">10_Approved PhD Evaluater Appointment Letter</a></li>
+                <li><a target="_blank" href="/assets/files/phd/11_Approved Ph.D Adjudication Report.docx">11_Approved Ph.D Adjudication Report</a></li>
+                <li><a target="_blank" href="/assets/files/phd/12_Approved PhD Evaluation of Thesis Letter.docx">12_Approved PhD Evaluation of Thesis Letter</a></li>
+                <li><a target="_blank" href="/assets/files/phd/13_Approved PhD Examination Remulneration Bill.docx">13_Approved PhD Examination Remulneration Bill</a></li>
+                <li><a target="_blank" href="/assets/files/phd/14_CHECK LIST T0 BE SUBMITTED WITH THESIS.docx">14_CHECK LIST T0 BE SUBMITTED WITH THESIS</a></li>
+               
+
+              </ul>
+            </div>
+<h2 className="universityobject-heading dark-mode-white-color py-4 rounded-tl-[40px] rounded-br-[40px] w-fit px-8 bg-[#4f70b6] font-bold text-white mx-auto mb-50">
+              PG. Section
+            </h2>
+
+            {/* ================= PDF SECTION ================= */}
+            <div className="tl-event-details-area rounded-lg mb-8 shadow-[2px_2px_0_3px_rgba(242,97,34,0.6)] p-4">
+              <h4 className="font-bold mb-3 text-[#4f70b6]">📄 PDF Files</h4>
+              <ul className="tl-event-details-key-content-list phd-admission-process">
+
+                <li><a target="_blank" href="/assets/files/pg/01_Approved Guidelines for PG Dissertation.pdf">01_Approved Guidelines for PG Dissertation</a></li>
+                <li><a target="_blank" href="/assets/files/pg/02_Approved PG Dissertation Template.pdf">02_Approved PG Dissertation Template</a></li>
+                <li><a target="_blank" href="/assets/files/pg/03_Approved PG Examination Remulneration Bill.pdf">03_Approved PG Examination Remulneration Bill</a></li>
+               
+              </ul>
+            </div>
+
+            {/* ================= WORD SECTION ================= */}
+            <div className="tl-event-details-area rounded-lg mb-8 shadow-[2px_2px_0_3px_rgba(79,112,182,0.6)] p-4">
+              <h4 className="font-bold mb-3 text-[#f26122]">📝 Word Files (DOCX)</h4>
+              <ul className="tl-event-details-key-content-list phd-admission-process">
+
+                <li><a target="_blank" href="/assets/files/pg/01_Approved Guidelines for Peparation of PhD Thesis.docx">01_Approved Guidelines for Peparation of PhD Thesis</a></li>
+                <li><a target="_blank" href="/assets/files/pg/02_Approved PG Dissertation Template.docx">02_Approved PG Dissertation Template</a></li>
+                <li><a target="_blank" href="/assets/files/pg/03_Approved PG Examination Remulneration Bill.docx">03_Approved PG Examination Remulneration Bill</a></li>
+                
+               
+
+              </ul>
+            </div>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PhdProgram;
