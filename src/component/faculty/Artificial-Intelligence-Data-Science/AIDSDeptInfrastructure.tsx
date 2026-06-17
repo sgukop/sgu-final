aa7acf03ee04e6344b32b388ed2a1b0d516d1e9d
sgukop <kiran.dhanawade@sguk.ac.in>
2026-06-17 18:44:22 +0530

    update AIDS

--- src/component/faculty/Artificial-Intelligence-Data-Science/AIDSDeptInfrastructure.tsx
+++ src/component/faculty/Artificial-Intelligence-Data-Science/AIDSDeptInfrastructure.tsx
@@ -18,77 +18,7 @@ const AIDSDeptInfrastructure= () => {
             data-offset="0"
           >
             <div className="row g-3 g-lg-4 justify-content-center">
-               <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure><img src="assets\images\AIDS\academic-infrastructure/1.jpg" alt="Infrastructure Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">5.0 Lab</h3>
-                    </div>
-                  </div>
-                 
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure><img src="assets\images\AIDS\academic-infrastructure/2.jpg" alt="Events Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Staffroom</h3>
-                    </div>
-                  </div>
-
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure><img src="assets\images\AIDS\academic-infrastructure/3.jpg" alt="Events Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Classroom 1</h3>
-                    </div>
-                  </div>
-
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure><img src="assets\images\AIDS\academic-infrastructure/4.jpg" alt="Events Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Classroom 2</h3>
-                    </div>
-                  </div>
-
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure><img src="assets\images\AIDS\academic-infrastructure/5.jpg" alt="Events Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Classroom 3</h3>
-                    </div>
-                  </div>
-
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure><img src="assets\images\AIDS\academic-infrastructure/8.jpg" alt="Events Image"/></figure>
-                        </div>S
-                        <h3 className="tl-6-latest-article-title mt-10">Classroom 4</h3>
-                    </div>
-                  </div>
-
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure> <img src="assets\images\AIDS\academic-infrastructure/7.jpeg" alt="Events Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Anenka Lab 1</h3>
-                      </div>
-                  </div>
-                  
-                  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-                    <div className="tl-9-blog flex flex-col h-full"> 
-                        <div className="tl-9-event-img">
-                            <figure><img src="assets\images\AIDS\academic-infrastructure/6.jpeg" alt="Events Image"/></figure>
-                        </div>
-                        <h3 className="tl-6-latest-article-title mt-10">Anenka Lab 2</h3>
-                      </div>
-                  </div>
+               
             </div>   
         </div>
        </div>
