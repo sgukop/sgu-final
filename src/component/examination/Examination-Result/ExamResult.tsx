d29930df60607dbed3839b4a65276ddc40152514
sgukop <kiran.dhanawade@sguk.ac.in>
2026-07-01 17:05:57 +0530

    exam notec

--- src/component/examination/Examination-Result/ExamResult.tsx
+++ src/component/examination/Examination-Result/ExamResult.tsx
@@ -3,106 +3,106 @@
 import React from "react";
 import Table from "react-bootstrap/Table";
 
-const pdfLink =
-  "https://manage-api.sguk.ac.in/api/assets/6a44eee6634f29742167a488";
-  "https://manage-api.sguk.ac.in/api/assets/6a44ef59634f29742167a493";
-  "https://manage-api.sguk.ac.in/api/assets/6a44ef6d634f29742167a498";
-  "https://manage-api.sguk.ac.in/api/assets/6a44ef82634f29742167a49d";
-  "https://manage-api.sguk.ac.in/api/assets/6a44ef9c634f29742167a4a2";
-  "https://manage-api.sguk.ac.in/api/assets/6a44efae634f29742167a4a7";
-  "https://manage-api.sguk.ac.in/api/assets/6a44efc2634f29742167a4ac";
-  "https://manage-api.sguk.ac.in/api/assets/6a44efd6634f29742167a4b1";
-  "https://manage-api.sguk.ac.in/api/assets/6a44eff6634f29742167a4b8";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f00d634f29742167a4bf";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f01e634f29742167a4c4";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f03e634f29742167a4c9";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f04e634f29742167a4ce";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f060634f29742167a4d3";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f072634f29742167a4d8";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f083634f29742167a4df";
-  "https://manage-api.sguk.ac.in/api/assets/6a44f095634f29742167a4e4";
-
+const pdfLinks = [
+  "https://manage-api.sguk.ac.in/api/assets/6a44eee6634f29742167a488",
+  "https://manage-api.sguk.ac.in/api/assets/6a44ef59634f29742167a493",
+  "https://manage-api.sguk.ac.in/api/assets/6a44ef6d634f29742167a498",
+  "https://manage-api.sguk.ac.in/api/assets/6a44ef82634f29742167a49d",
+  "https://manage-api.sguk.ac.in/api/assets/6a44ef9c634f29742167a4a2",
+  "https://manage-api.sguk.ac.in/api/assets/6a44efae634f29742167a4a7",
+  "https://manage-api.sguk.ac.in/api/assets/6a44efc2634f29742167a4ac",
+  "https://manage-api.sguk.ac.in/api/assets/6a44efd6634f29742167a4b1",
+  "https://manage-api.sguk.ac.in/api/assets/6a44eff6634f29742167a4b8",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f00d634f29742167a4bf",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f01e634f29742167a4c4",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f03e634f29742167a4c9",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f04e634f29742167a4ce",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f060634f29742167a4d3",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f072634f29742167a4d8",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f083634f29742167a4df",
+  "https://manage-api.sguk.ac.in/api/assets/6a44f095634f29742167a4e4",
+];
 
 const results = [
   {
     date: "01-06-2026",
     title: "Result Declaration Summer-2026 Notice-01-06-2026-1",
-    link: pdfLink[0],
+    link: pdfLinks[0],
   },
   {
     date: "01-06-2026",
     title: "Result Declaration Notice",
-    link: pdfLink[1],
+    link: pdfLinks[1],
   },
   {
     date: "01-06-2026",
     title: "CSE Non-NEP Curriculum",
-    link: pdfLink[2],
+    link: pdfLinks[2],
   },
   {
     date: "01-06-2026",
     title: "AIML & B.Pharmacy",
-    link: pdfLink[3],
+    link: pdfLinks[3],
   },
   {
     date: "01-06-2026",
     title: "Aero, Civil, Mechanical & Electrical",
-    link: pdfLink[4],
+    link: pdfLinks[4],
   },
   {
     date: "01-06-2026",
     title: "FST & MLT",
-    link: pdfLink[5],
+    link: pdfLinks[5],
   },
   {
     date: "01-06-2026",
     title: "B.Pharmacy & Civil Engineering",
-    link: pdfLink[6],
+    link: pdfLinks[6],
   },
   {
     date: "01-06-2026",
     title: "B.Sc Biotechnology",
-    link: pdfLink[7],
+    link: pdfLinks[7],
   },
   {
     date: "01-06-2026",
     title: "LLB",
-    link: pdfLink[8],
+    link: pdfLinks[8],
   },
   {
     date: "01-06-2026",
     title: "B.Design",
-    link: pdfLink[9],
+    link: pdfLinks[9],
   },
   {
     date: "01-06-2026",
     title: "BBA / CRT / BCA / CSE / Design",
-    link: pdfLink[10],
+    link: pdfLinks[10],
   },
   {
     date: "01-06-2026",
     title: "BA",
-    link: pdfLink[11],
+    link: pdfLinks[11],
   },
   {
     date: "01-06-2026",
     title: "BBA",
-    link: pdfLink[12],
+    link: pdfLinks[12],
   },
   {
     date: "01-06-2026",
     title: "B.Sc Chemistry",
-    link: pdfLink[13],
+    link: pdfLinks[13],
   },
   {
     date: "01-06-2026",
     title: "BJMC",
-    link: pdfLink[14],
+    link: pdfLinks[14],
   },
   {
     date: "01-06-2026",
     title: "M.Pharm",
-    link: pdfLink[15],
+    link: pdfLinks[15],
   },
 ];
 
@@ -122,11 +122,9 @@ const ExamResult = () => {
           <Table bordered hover responsive className="governing-table">
             <thead>
               <tr>
-                
+                <th>Date</th>
                 <th>Notice</th>
-                <th  className="text-center">
-                  Download
-                </th>
+                <th className="text-center">Download</th>
               </tr>
             </thead>
 
