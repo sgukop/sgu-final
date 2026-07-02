f46eeda1e70703551b8f7b9a4f42b490d8041663
sgukop <kiran.dhanawade@sguk.ac.in>
2026-07-02 16:22:59 +0530

    op

--- src/app/Blog/page.tsx
+++ src/app/Blog/page.tsx
@@ -1,4 +1,5 @@
 import Blog from '@/component/Blog/Media'
+import BlogSection9 from '@/component/blog/BlogSection9'
 import BreadcrumbSection from '@/component/breadcrumb/BreadcrumbSection'
 import CtaSection4 from '@/component/cta/CtaSection4'
 import Layout5 from '@/component/layout/Layout5'
