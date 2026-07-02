a96c9d6909bc5e251535776797811dcb4dec3af9
sgukop <kiran.dhanawade@sguk.ac.in>
2026-07-02 15:10:52 +0530

    blog2

---
+++ src/app/Blog/page.tsx
@@ -0,0 +1,23 @@
+import Blog from '@/component/Blog/Media'
+import BreadcrumbSection from '@/component/breadcrumb/BreadcrumbSection'
+import CtaSection4 from '@/component/cta/CtaSection4'
+import Layout5 from '@/component/layout/Layout5'
+import PricingSection from '@/component/pricing/PricingSection'
+import { Metadata } from 'next'
+import React from 'react'
+export const metadata: Metadata = {
+    title: 'SGU University',
+    description: 'Developed by Enark',
+  }
+const BlogPage = () => {
+  return (
+    <Layout5>
+        <BreadcrumbSection title='New & Blog @ SGU' style=''/>
+        <Blog />
+       {/* <PricingSection/>
+        <CtaSection4 style='tl-8-cta-inner'/> */}
+    </Layout5>
+  )
+}
+
+export default BlogPage
