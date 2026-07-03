6c2b0fd47c8f4d7088b33bf53cf5cfe42a12fffd
sgukop <kiran.dhanawade@sguk.ac.in>
2026-07-03 11:36:06 +0530

    update

--- src/app/home-10/page.tsx
+++ src/app/home-10/page.tsx
@@ -1,7 +1,7 @@
 import AboutSection9 from '@/component/about/AboutSection9'
 import BannerSection9 from '@/component/banner/BannerSection9'
 // import BlogSection6 from '@/component/blog/BlogSection6';
-import BlogSection9 from '@/component/blog/BlogSection9';
+//import BlogSection9 from '@/component/blog/BlogSection9';
 import CampusSection2 from '@/component/campus/CampusSection2';
 import CourseSection2 from '@/component/course/CourseSection2';
 import FooterSection7 from '@/component/footer/FooterSection7';
@@ -32,7 +32,7 @@ const Home10 = () => {
         <CampusSection2 />
         <TestimonialSection2 />
         <PricingSection3 />
-        <BlogSection9 />
+        {/* <BlogSection9 /> */}
         <FooterSection7 style='' logo='assets/images/logos/logo-4-light.png' descStyle='tl-footer-descr' socialStyle='tl-footer-socials'/>
         <VideoModal />
     </div>
