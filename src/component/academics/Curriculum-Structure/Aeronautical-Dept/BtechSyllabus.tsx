b94fb7a25138bb6c3d1dce687535b859ffba8199
sgukop <kiran.dhanawade@sguk.ac.in>
2026-07-09 16:39:35 +0530

    Aero Syllabus

--- src/component/academics/Curriculum-Structure/Aeronautical-Dept/BtechSyllabus.tsx
+++ src/component/academics/Curriculum-Structure/Aeronautical-Dept/BtechSyllabus.tsx
@@ -13,7 +13,7 @@ const BtechSyllabus = () => {
               </tr>
           </thead>
           <tbody>
-               <tr>
+               {/*<tr>
                   <td><p>Curriculum Structure Sem I-VIII R25</p></td>
                   <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68c2b84bdceab808938fd380">Curriculum  Structure Sem I-VIII R25<i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
@@ -40,11 +40,28 @@ const BtechSyllabus = () => {
              <tr>
                   <td><p>Minor Aero Structure and Syllabus R23</p></td>
                   <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68834a05153ff71942442972">Minor Aero Structure and Syllabus R23 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+             </tr>*/}
+             <tr>
+                  <td><p>B.Tech. Aero. Engg. Strucutre R25</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/6a4f80be48b1d3bae2f5d1de">B.Tech. Aero. Engg. Strucutre R25 <i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
              <tr>
-                  <td><p>Minor Aero Structure and Syllabus R25</p></td>
-                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/68c2b920dceab808938fd39e">Minor Aero Structure and Syllabus R25 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+                  <td><p>B. Tech. Aero. Engg. F. Y. Syllabus R25</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/6a4f80d248b1d3bae2f5d1e9">B. Tech. Aero. Engg. F. Y. Syllabus R25 <i className="fa-regular fa-arrow-right-long"></i></a></td>
              </tr>
+             <tr>
+                  <td><p>B. Tech. Aero. Engg. S. Y. Syllabus R25</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/6a4f80df48b1d3bae2f5d1f0">B. Tech. Aero. Engg. S. Y. Syllabus R25 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+             </tr>
+             <tr>
+                  <td><p>B.Tech. Aer. EngG. Strucutre R23</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/6a4f80ed48b1d3bae2f5d1f7">B.Tech. Aer. EngG. Strucutre R23 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+             </tr>
+             <tr>
+                  <td><p>B. Tech. Aero. Engg. Syllabus Sem VII and VIII R23</p></td>
+                  <td><a rel="noopener noreferrer" className="tl-def-btn-2" target="_blank" href="https://manage-api.sguk.ac.in/api/assets/6a4f80f948b1d3bae2f5d1fc">B. Tech. Aero. Engg. Syllabus Sem VII and VIII R23 <i className="fa-regular fa-arrow-right-long"></i></a></td>
+             </tr>
+            
              
               
           </tbody>
