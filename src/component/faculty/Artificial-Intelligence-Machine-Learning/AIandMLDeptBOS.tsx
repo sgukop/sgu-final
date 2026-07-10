ec6fb06fd9597e575a0296f40d8caebc604f092a
sgukop <kiran.dhanawade@sguk.ac.in>
2026-07-10 15:26:06 +0530

    update BOS

--- src/component/faculty/Artificial-Intelligence-Machine-Learning/AIandMLDeptBOS.tsx
+++ src/component/faculty/Artificial-Intelligence-Machine-Learning/AIandMLDeptBOS.tsx
@@ -5,125 +5,224 @@ import Table from "react-bootstrap/Table";
 import NavLinks from "./Navlinks";
 import MenuList from "../Reusable-components/MenuList";
 
-function AIandMLDeptBOS() {
+function AIandMLFacultyList() {
   return (
     <div className="pt-100 pb-100">
       <div className="container-fluid">
-            <div className="governing-body">
-                <div className="d-flex justify-content-center"><h2 className="tl-2-section-title mb-50">BOS Members List</h2></div>
-            </div>
-            <div className="row">
-                <MenuList dept="Artificial Intelligence and Machine Learning" subMenu={NavLinks} />
-            <div
+        <div className="governing-body">
+          <div className="d-flex justify-content-center">
+            <h2 className="tl-2-section-title mb-50">Faculty Members List</h2>
+          </div>
+        </div>
+        <div className="row">
+          <MenuList dept="Artificial Intelligence and Machine Learning" subMenu={NavLinks} />
+          <div
             data-spy="scroll"
             className="scrollspy-example z-depth-1 mt-4 p-3 mb-4 col-lg-9 col-md-6 col-sm-12 dept-content"
             data-target="#menu-navbar"
             data-offset="0"
           >
-        <div className="row gx-0 tl-blog-details-row">
-          <div className="col-lg-12">
-            
+            <div className="row gx-0 tl-blog-details-row">
+              <div className="col-lg-12">
+                <Table striped bordered hover responsive className="governing-table">
+                  <thead>
+                    <tr>
+                      <th><p className="text-[#4f70b6]">Sr.No</p></th>
+                      <th><p className="text-[#4f70b6]">Name of the Member</p></th>
+                      <th><p className="text-[#4f70b6]">Post</p></th>
+                      <th><p className="text-[#4f70b6]">Designation</p></th>
+                      <th><p className="text-[#4f70b6]">Mail ID</p></th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td><p>1</p></td>
+                      <td><p>Dr. Santaji Shinde</p></td>
+                      <td><p>HOD & Professor</p></td>
+                      <td><p>Chairman</p></td>
+                      <td>
+                        <a href="mailto:santaji.shinde@am.sguk.ac.in" className="tl-def-btn-2">
+                          santaji.shinde@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-            <Table striped bordered hover responsive className="governing-table">
-              <thead>
-                <tr>
-                  <th><p className="text-[#4f70b6]">Sr.No</p></th>
-                  <th><p className="text-[#4f70b6]">Name of the Member</p></th>
-                  <th><p className="text-[#4f70b6]">Post</p></th>
-                  <th><p className="text-[#4f70b6]">Designation</p></th>
-                  <th><p className="text-[#4f70b6]">Mail ID  and Mobile No</p></th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td><p>1</p></td>
-                  <td><p>Dr. Mahesh P. Gaikwad </p></td>
-                  <td><p>Assoc.Professor and HOD,AIML</p></td>
-                  <td><p>Chairman</p></td>
-                  <td><a href="mailto:hod.aiml@sanjayghodawatuniversity.ac.in" className="tl-def-btn-2">hod.aiml@sanjayghodawatuniversity.ac.in <i className="fa-regular fa-arrow-right"></i></a><p>9766802914</p></td>
-                </tr>
+                    <tr>
+                      <td><p>2</p></td>
+                      <td><p>Dr. Pradeep B.Mane</p></td>
+                      <td><p>Principal</p></td>
+                      <td><p>Member (Expert Researcher)</p></td>
+                      <td>
+                        <a href="mailto:Pbmane6829@gmail.com" className="tl-def-btn-2">
+                          Pbmane6829@gmail.com <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>2</p></td>
-                  <td><p>Dr. Pradeep B.Mane</p></td>
-                  <td><p>Principal</p></td>
-                  <td><p>Member (Expert Researcher)</p></td>
-                  <td><a href="mailto:Pbmane6829@gmail.com" className="tl-def-btn-2">Pbmane6829@gmail.com <i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9420918444</p></td>
-                </tr>
+                    <tr>
+                      <td><p>3</p></td>
+                      <td><p>Dr. Shruti Patil</p></td>
+                      <td><p>Assoc Professor</p></td>
+                      <td><p>Member (Expert Academician)</p></td>
+                      <td>
+                        <a href="mailto:headaiml@sitpune.edu.in" className="tl-def-btn-2">
+                          headaiml@sitpune.edu.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>3</p></td>
-                  <td><p>Dr. Shruti Patil</p></td>
-                  <td><p>Assoc Professor</p></td>
-                  <td><p>Member (Expert Academician)</p></td>
-                  <td><a href="mailto:headaiml@sitpune.edu.in" className="tl-def-btn-2">headaiml@sitpune.edu.in <i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9823038771</p></td>
-                </tr>
+                    <tr>
+                      <td><p>4</p></td>
+                      <td><p>Dr. Sujata S. Kulkarni</p></td>
+                      <td><p>Assoc Professor</p></td>
+                      <td><p>Member (Expert Academician)</p></td>
+                      <td>
+                        <a href="mailto:taresujata@yahoo.com" className="tl-def-btn-2">
+                          taresujata@yahoo.com <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>4</p></td>
-                  <td><p>Dr. Sujata S. Kulkarni</p></td>
-                  <td><p>Assoc Professor</p></td>
-                  <td><p>Member (Expert Academician)</p></td>
-                  <td><a href="mailto:taresujata@yahoo.com" className="tl-def-btn-2">taresujata@yahoo.com<i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9920899023</p></td>
-                </tr>
+                    <tr>
+                      <td><p>5</p></td>
+                      <td><p>Dr. Priyanka Shivaprasad More</p></td>
+                      <td><p>Associate Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:priyanka.more@am.sguk.ac.in" className="tl-def-btn-2">
+                          priyanka.more@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>5</p></td>
-                  <td><p>Amrish Patil</p></td>
-                  <td><p>Assistant Professor</p></td>
-                  <td><p>Member</p></td>
-                  <td><a href="mailto:amrish.patil@sanjayghodawatuniversity.ac.in" className="tl-def-btn-2">amrish.patil@sanjayghodawatuniversity.ac.in<i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9970221308</p></td>
-                </tr>
+                    <tr>
+                      <td><p>6</p></td>
+                      <td><p>Mrs. Pallavi Amol Majgave</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:pallavi.majgave@am.sguk.ac.in" className="tl-def-btn-2">
+                          pallavi.majgave@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>6</p></td>
-                  <td><p>Varsha Kasote</p></td>
-                  <td><p>Assistant Professor</p></td>
-                  <td><p>Member</p></td>
-                  <td><a href="mailto:varsha.kasote@sanjayghodawatuniversity.ac.in" className="tl-def-btn-2">varsha.kasote@sanjayghodawatuniversity.ac.in<i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9511760292</p></td>
-                </tr>
+                    <tr>
+                      <td><p>7</p></td>
+                      <td><p>Mr. Amrish Patil</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:amrish.patil@am.sguk.ac.in" className="tl-def-btn-2">
+                          amrish.patil@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>7</p></td>
-                  <td><p>Pallavi Majgave</p></td>
-                  <td><p>Assistant Professor</p></td>
-                  <td><p>Member</p></td>
-                  <td><a href="mailto:pallavi.majgave@sanjayghodawatuniversity.ac.in" className="tl-def-btn-2">pallavi.majgave@sanjayghodawatuniversity.ac.in<i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9822170623</p></td>
-                </tr>
+                    <tr>
+                      <td><p>8</p></td>
+                      <td><p>Mrs. Varsha Kunal Kasote</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:varsha.kasote@am.sguk.ac.in" className="tl-def-btn-2">
+                          varsha.kasote@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>8</p></td>
-                  <td><p>Vidyananad Upadhye</p></td>
-                  <td><p>Assistant Professor</p></td>
-                  <td><p>Member</p></td>
-                  <td><a href="mailto:vidyananad.upadhye@sanjayghodawatuniversity.ac.in" className="tl-def-btn-2">vidyananad.upadhye@sanjayghodawatuniversity.ac.in<i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9881446349</p></td>
-                </tr>
+                    <tr>
+                      <td><p>9</p></td>
+                      <td><p>Mr. Najim Abdul Naik</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:najim.naik@am.sguk.ac.in" className="tl-def-btn-2">
+                          najim.naik@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-                <tr>
-                  <td><p>9</p></td>
-                  <td><p>Mrs. Shweta G. Shete</p></td>
-                  <td><p>Assistant Professor</p></td>
-                  <td><p>Member Secretary</p></td>
-                  <td><a href="mailto:shweta.shete@sanjayghodawatuniversity.ac.in" className="tl-def-btn-2">shweta.shete@sanjayghodawatuniversity.ac.in<i className="fa-regular fa-arrow-right"></i></a>
-                  <p>9503716550</p></td>
-                </tr>
+                    <tr>
+                      <td><p>10</p></td>
+                      <td><p>Mrs. Savita Charane</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:savita.charane@am.sguk.ac.in" className="tl-def-btn-2">
+                          savita.charane@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
 
-            </tbody>
-            </Table>
+                    <tr>
+                      <td><p>11</p></td>
+                      <td><p>Mr. Jahir Shaikh</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:jahir.shaikh@am.sguk.ac.in" className="tl-def-btn-2">
+                          jahir.shaikh@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><p>12</p></td>
+                      <td><p>Mr. Vinayak Kishor Kumbhar</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:vinayak.kumbhar@am.sguk.ac.in" className="tl-def-btn-2">
+                          vinayak.kumbhar@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><p>13</p></td>
+                      <td><p>Mr. Kamble Siyang Prafulla</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:kamble.prafulla@am.sguk.ac.in" className="tl-def-btn-2">
+                          kamble.prafulla@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><p>14</p></td>
+                      <td><p>Mrs. Reshma Vishal Pasale</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member</p></td>
+                      <td>
+                        <a href="mailto:reshma.pasale@am.sguk.ac.in" className="tl-def-btn-2">
+                          reshma.pasale@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
+
+                    <tr>
+                      <td><p>15</p></td>
+                      <td><p>Ms. Shweta Shete</p></td>
+                      <td><p>Assistant Professor</p></td>
+                      <td><p>Member Secretary</p></td>
+                      <td>
+                        <a href="mailto:shweta.shete@am.sguk.ac.in" className="tl-def-btn-2">
+                          shweta.shete@am.sguk.ac.in <i className="fa-regular fa-arrow-right"></i>
+                        </a>
+                      </td>
+                    </tr>
+                  </tbody>
+                </Table>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
-      </div>
       </div>
     </div>
   );
 }
 
-export default AIandMLDeptBOS;
+export default AIandMLFacultyList;
--- src/component/faculty/Artificial-Intelligence-Machine-Learning/AIandMLDeptFaculty.tsx
+++ src/component/faculty/Artificial-Intelligence-Machine-Learning/AIandMLDeptFaculty.tsx
@@ -46,21 +46,7 @@ const AIandMLDeptFaculty = () => {
                             </div>
                         </div>
                     </div>  
-                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/AIML/2. Dr. AISHWARYA M. P..jpg" alt="Faculty Image"/>
-                            </div>
-
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Dr. Aishwarya M. P.</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Assistant Professor</p>
-                                <Link href="https://manage-api.sguk.ac.in/api/assets/67d939824f56d3a0bcfba034" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div>  
-
-                    
+                                      
 
                     <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
                         <div className="tl-1-course-new">
@@ -118,33 +104,7 @@ const AIandMLDeptFaculty = () => {
                         </div>
                     </div>  
                     
-                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/AIML/10. Ms. YOGITA PATIL.jpg" alt="Faculty Image"/>
-                            </div>
-
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Ms. Yogita Namdev Patil</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Assistant Professor</p>
-                                <Link href="https://manage-api.sguk.ac.in/api/assets/674ede594b17e19d8f3617c6" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div>  
-
-                     <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/AIML/11. Mr. VIDYANAND UPADHYE.jpg" alt="Faculty Image"/>
-                            </div>
-
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Mr. Vidyanand Ashok Upadhye</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Teaching Assistant</p>
-                                <Link href="https://manage-api.sguk.ac.in/api/assets/674ede6a4b17e19d8f3617cb" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div> 
+                        
                     <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
                         <div className="tl-1-course-new">
                             <div className="tl-1-course-img-new">
@@ -223,19 +183,7 @@ const AIandMLDeptFaculty = () => {
                             </div>
                         </div>
                     </div>
-                    <div className="col-md-12 col-lg-4 col-xxs-12 col-10">
-                        <div className="tl-1-course-new">
-                            <div className="tl-1-course-img-new">
-                                <img src="/assets/images/faculty-profile-photos/female.jpg" alt="Faculty Image"/>
-                            </div>
-
-                            <div className="tl-1-course-txt">
-                                <h4 className="tl-1-course-title-new mt-30">Ms.Snehlata Chaudhary</h4>
-                                <p className="tl-1-course-author-new dark-mode-white-color">Assistant Professor</p>
-                                <Link href="#" rel="noopener noreferrer" target="_blank" className="tl-3-def-btn"> Profile </Link>
-                            </div>
-                        </div>
-                    </div>
+                
                     
                 </div>
             </div> 
