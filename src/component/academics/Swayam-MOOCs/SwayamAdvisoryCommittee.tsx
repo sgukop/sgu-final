d20076441f7d7d05947cedd9813414b1217fc788
Your Name <your_kiran.dhanawade@sguk.ac.in>
2026-09-23 12:13:27 +0530

    sawmi

--- src/component/academics/Swayam-MOOCs/SwayamAdvisoryCommittee.tsx
+++ src/component/academics/Swayam-MOOCs/SwayamAdvisoryCommittee.tsx
@@ -46,15 +46,15 @@ function SwayamAdvisoryCommittee() {
 
                 <tr>
                     <td><p>4</p></td>
-                    <td><p>Dr. Balgaonkar</p></td>
-                    <td><p>Asso. Prof. Dept. of Management</p></td>
+                    <td><p>Dr. P. T. Kumbhar </p></td>
+                    <td><p>Asst. Prof. Management </p></td>
                     <td><p>Member</p></td>
                 </tr>
 
                 <tr>
                     <td><p>5</p></td>
-                    <td><p>Mr. N. V. Sabnis</p></td>
-                    <td><p>Asso. Prof. Mechanical Engg.</p></td>
+                    <td><p>Mr. Gurunath Machhale</p></td>
+                    <td><p>Asst. Prof. Comp. Sc. & Engg.</p></td>
                     <td><p>Nodal officer</p></td>
                 </tr>
 
@@ -67,83 +67,101 @@ function SwayamAdvisoryCommittee() {
 
                 <tr>
                     <td><p>6</p></td>
-                    <td><p>Mr. Syed  Habeeb</p></td>
+                    <td><p>Miss. Vaishnavi Patil </p></td>
                     <td><p>Aerospace Engineering</p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>7</p></td>
-                    <td><p>Mr. Nitish Patil</p></td>
-                    <td><p>Civil Engineering</p></td>
+                    <td><p>Mr. Zahir Shaikh </p></td>
+                    <td><p>Asst. Prof. AIML </p></td>
                     <td><p>Member</p></td>
                 </tr>
 
                 <tr>
                     <td><p>8</p></td>
-                    <td><p>Mr. Anand Kumbhar</p></td>
-                    <td><p>Electrical Engineering</p></td>
+                    <td><p>Ms. Srushti Patil</p></td>
+                    <td><p>Asst. Prof. Computer Science & Engineering </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>9</p></td>
-                    <td><p>Dr. Patil Abhijit</p></td>
-                    <td><p>Mechanical Engineering</p></td>
+                    <td><p>Mr. Nitish M Patil</p></td>
+                    <td><p>Asst. Prof. Civil Engg.</p></td>
                     <td><p>Member</p></td>
                 </tr>
 
                 <tr>
                     <td><p>10</p></td>
-                    <td><p>Ms. Aishwarya M. P</p></td>
-                    <td><p>Artificial Intelligence & Machine Learning</p></td>
+                    <td><p>Mrs. Shubhangi Deshnukh </p></td>
+                    <td><p>Asst. Prof. Electronics & Communication Engg. </p></td>
                     <td><p>Member</p></td>
                 </tr>
 
                 <tr>
                     <td><p>11</p></td>
-                    <td><p>Ms. Srushti Patil</p></td>
-                    <td><p>Computer Science and Eng.</p></td>
+                    <td><p>Ms. Pratiksha Talewadikar </p></td>
+                    <td><p>Asst. Prof. Computer Application </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>12</p></td>
-                    <td><p>Ms. Swati  Patil</p></td>
-                    <td><p>Computer Application</p></td>
+                    <td><p>Dr. Satish Mahadik </p></td>
+                    <td><p>Asst. Prof. Physics </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>13</p></td>
                     <td><p>Ms. Monika Chavan</p></td>
-                    <td><p>School of Physical and Chemical Sciences</p></td>
+                    <td><p>Asst. Prof. Chemistry </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>14</p></td>
-                    <td><p>Ms. Minakshi Chauthamahal</p></td>
-                    <td><p>School of Commerce and Management</p></td>
+                    <td><p>Dr. Ajay Nalawade </p></td>
+                    <td><p>Asst. Prof. Biotech. </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>15</p></td>
-                    <td><p>Ms. Sharvari Madhale</p></td>
-                    <td><p>School of Media</p></td>
+                    <td><p>Ms.Vishakha Deshpande</p></td>
+                    <td><p>Asst. Prof. Food Sc. & Tech. </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>16</p></td>
-                    <td><p>Ms. Deepali Suryavanshi</p></td>
-                    <td><p>School of Pharmaceutical Sciences</p></td>
+                    <td><p>Dr. Jyotsna Patil </p></td>
+                    <td><p>Asst. Prof. Medical Lab Technology </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>17</p></td>
-                    <td><p>Mrs. Jyoti Waykule</p></td>
-                    <td><p>Electronics and Communication Eng.</p></td>
+                    <td><p>Mr. Milind Shinde</p></td>
+                    <td><p>Asst. Prof. Management </p></td>
                     <td><p>Member</p></td>
                 </tr>
                 <tr>
                     <td><p>18</p></td>
-                    <td><p>Ms. Rasika Kandale</p></td>
-                    <td><p>School of Design</p></td>
+                    <td><p>Mr. Prafull Prakash Patil</p></td>
+                    <td><p>Asst. Prof. Pharmacy </p></td>
+                    <td><p>Member</p></td>
+                </tr>
+                <tr>
+                    <td><p>19</p></td>
+                    <td><p>Mr. Sayyad Irfan Merajali </p></td>
+                    <td><p>Asst. Prof. Clinical Research Technology  </p></td>
+                    <td><p>Member</p></td>
+                </tr>
+                <tr>
+                    <td><p>20</p></td>
+                    <td><p>Adv. Anagha Sutar</p></td>
+                    <td><p>Asst. Prof. Law </p></td>
+                    <td><p>Member</p></td>
+                </tr>
+                <tr>
+                    <td><p>21</p></td>
+                    <td><p>Mr. Prathmesh Kate </p></td>
+                    <td><p>Asst. Prof. Comp. Sc. & Engg. </p></td>
                     <td><p>Member</p></td>
                 </tr>
               </tbody>
