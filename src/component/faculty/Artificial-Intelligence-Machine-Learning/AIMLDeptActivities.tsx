9a0d255f3c326bcbe84dcca24461b900bb4ca5b2
Your Name <your_kiran.dhanawade@sguk.ac.in>
2026-09-25 15:33:45 +0530

    up

--- src/component/faculty/Artificial-Intelligence-Machine-Learning/AIMLDeptActivities.tsx
+++ src/component/faculty/Artificial-Intelligence-Machine-Learning/AIMLDeptActivities.tsx
@@ -19,18 +19,22 @@ const AIMLDeptActivities = () => {
           >
 
                 <h3 className="tl-event-details-area-title">
-                Students Achieve Historic Success at Capgemini Hackathon
-              </h3>
+  Students Achieve Historic Success at Capgemini Hackathon
+</h3>
 
-               <p className="dark-mode-white-color text-justify">
-             The proudly congratulates its students for their outstanding achievement at the Capgemini Hackathon  Pune Zone Finale, securing 1st Prize, 2nd Prize and Runner-Up and achieving a historic clean sweep of all three top positions.
-                </p>
+<p className="dark-mode-white-color text-justify">
+  The proudly congratulates its students for their outstanding achievement
+  at the Capgemini Hackathon – Pune Zone Finale, securing 1st Prize,
+  2nd Prize and Runner-Up and achieving a historic clean sweep of all
+  three top positions.
+</p>
 
-              <div className="row g-3 g-lg-4 lift-content-center">
-            <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
-              <div className="tl-9-blog">
-                <div className="tl-9-event-img">
-               <figure>
+<div className="row g-3 g-lg-4 lift-content-center">
+
+  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
+    <div className="tl-9-blog">
+      <div className="tl-9-event-img">
+        <figure>
           <img
             src="assets/images/AIML/activities&achievements/1.jpeg"
             alt="Students at Capgemini Hackathon"
@@ -52,8 +56,8 @@ const AIMLDeptActivities = () => {
       </div>
     </div>
   </div>
-            </div>
-<div className="col-lg-4 col-md-6 col-10 col-xxs-12">
+
+  <div className="col-lg-4 col-md-6 col-10 col-xxs-12">
     <div className="tl-9-blog">
       <div className="tl-9-event-img">
         <figure>
@@ -65,6 +69,8 @@ const AIMLDeptActivities = () => {
       </div>
     </div>
   </div>
+
+</div>
             
                 <h3 className="tl-event-details-area-title">F.Y. AIML Orientation</h3>  
                <div className="row g-3 g-lg-4 lift-content-center">
